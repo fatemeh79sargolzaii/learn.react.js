b9ddfa04fdaf30cdb72c48a684bd2b87d59004cb
fatemeh.sargolzai <78509827+fatemeh79sargolzaii@users.noreply.github.com>
2021-09-24 20:21:22 +0330

    learn-react

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+   <div className="ui fixed menu">
+      <div className="ui container d-flex justify-content-center" >
+              <h2 style={{fontFamily:'Koh Santepheap'}}>Contact Manager</h2>
+          </div>
+      </div>
+    )
+}
+
+export default Header
